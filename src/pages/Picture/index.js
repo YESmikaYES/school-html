0d0031aa14696c40e3c1d5bdfd6c11de81a5dbb5
Mika Klimek <klimek.mika@gmail.com>
2023-10-25 11:58:46 +0200

    :sparkles: adds image link and titles

--- src/pages/Picture/index.js
+++ src/pages/Picture/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import "./style.css";
 
 export default () => (
-    <div id="container">
+    <div>
         <br />
         <br />
         <br />
         <h3>Local Storage Pictures</h3>
         <div className="row">
-        <img className="item" src="/assets/solar.png"></img>
-        <img className="item" src="/assets/arc.png"></img>
-        <img className="item" src="/assets/void.png"></img>
-        <img className="item" src="/assets/stasis.png"></img>
-        <img className="item" src="/assets/strand.png"></img>
+            <img className="item" src="/assets/solar.png" title="solar titan"></img>
+            <img className="item" src="/assets/arc.png" title="arc warlock"></img>
+            <img className="item" src="/assets/void.png" title="void  hunter"></img>
+            <img className="item" src="/assets/stasis.png" title="stasis titan"></img>
+            <img className="item" src="/assets/strand.png" title="strand warlock"></img>
         </div>
         <br />
         <br />
@@ -34,8 +34,9 @@ export default () => (
             <img className="item" src="https://imgs.search.brave.com/z1uvEjdsoWbG8BdI3Ou2aUP8l-_YRYT0Ce0k94Yp-bI/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pMC53/cC5jb20vZXJyb3Jm/aXhlci5jby93cC1j/b250ZW50L3VwbG9h/ZHMvMjAxOS8xMC9j/aGFuZ2Utc2VydmVy/LWluLWRlc3Rpbnkt/Mi5qcGc_cmVzaXpl/PTg0MCw0NzMmc3Ns/PTE"></img>
             <img className="item" src="https://imgs.search.brave.com/NPU6o4x-y1uXc7y3itl5gD3S0zIwNrPCW7tdjNXLAFQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90d2lu/ZmluaXRlLm5ldC93/cC1jb250ZW50L3Vw/bG9hZHMvMjAyMC8x/MS9kZXN0aW55LTIt/YmV5b25kLWxpZ2h0/LWd1aWRlLXdpa2ku/anBnP2ZpdD0xMjAw/LDY3NQ"></img>
             <img className="item" src="https://imgs.search.brave.com/viPbqLakxmH9Ou3tMqQUsIsXxDGAevRiwPnp2Bds1NY/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/bW9zLmNtcy5mdXR1/cmVjZG4ubmV0L0Rm/WldtOG5hZFRUc0th/RUdjQVRYckMtMzIw/LTgwLmpwZw"></img>
+            <a id="link" href="https://www.youtube.com/watch?v=6L3mH4qW1d8">
             <img className="item" src="https://imgs.search.brave.com/apHdOLjvVvwzS6mhKZvj8V7q9q4B6umn1CManFGALX8/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9zdGF0/aWMud2lraWEubm9j/b29raWUubmV0L2Rl/c3RpbnlwZWRpYS9p/bWFnZXMvNi82ZC9E/ZXN0aW55XzJfRm9y/c2FrZW5fJUUyJTgw/JTkzX09mZmljaWFs/X0dhbWJpdF9UcmFp/bGVyL3JldmlzaW9u/L2xhdGVzdC9zY2Fs/ZS10by13aWR0aC1k/b3duLzE4NT9jYj0y/MDE4MDgyMjE4MDI1/Mw"></img>
-
+            </a>
         </div>
     </div>
 )
--- src/pages/Table/index.js
+++ src/pages/Table/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css"
 
 export default () => (
-    <div id="container">
+    <div>
       <br />
       <br />
       <br />
@@ -24,38 +24,38 @@ export default () => (
           </tr>
 
           <tr className="solar">
-            <td id="solar">Solar</td>
-            <td>Dawnblade</td>
-            <td>Sunbreaker</td>
-            <td>Gunslinger</td>
+            <td id="solar" title="fire">Solar</td>
+            <td title="born to nova forced to well">Dawnblade</td>
+            <td title="bonk">Sunbreaker</td>
+            <td title="golden gun go brrr">Gunslinger</td>
 
           </tr>
           <tr className="arc">
-            <td id="arc">Arc</td>
-            <td>Stormcaller</td>
-            <td>Striker</td>
-            <td>Arcstrider</td>
+            <td id="arc" title="lightning">Arc</td>
+            <td title="meme beam">Stormcaller</td>
+            <td title="kys">Striker</td>
+            <td title="reflectevery super">Arcstrider</td>
 
           </tr>
           <tr className="void">
-            <td id="void">Void</td>
-            <td>Voidwalker</td>
-            <td>Defender</td>
-            <td>Nightstalker</td>
+            <td id="void" title="lean">Void</td>
+            <td title="stupid purple magic go brr">Voidwalker</td>
+            <td title="bubble is  stupid">Defender</td>
+            <td title="invis is totally fair and balanced">Nightstalker</td>
 
           </tr>
           <tr className="stasis">
-            <td id="stasis">Stasis</td>
-            <td>Shadebinder</td>
-            <td>Behemoth</td>
-            <td>Revenant</td>
+            <td id="stasis" title="ice">Stasis</td>
+            <td title="why does everyone only care about stasis turrets">Shadebinder</td>
+            <td title="a shadow of its former glory">Behemoth</td>
+            <td title="rip shitterdive you wont be missed">Revenant</td>
 
           </tr>
           <tr className="strand">
-            <td id="strand">Strand</td>
-            <td>Broodweaver</td>
-            <td>Berserker</td>
-            <td>Threadrunner</td>
+            <td id="strand" title="strings">Strand</td>
+            <td title="W O R M">Broodweaver</td>
+            <td title="suspend has been nerfed LLLLL">Berserker</td>
+            <td title="shitterdive but balanced PogU">Threadrunner</td>
 
           </tr>
         </table>
